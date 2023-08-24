6cce1023779b62140e27011d24109ff3b200b18e
Olga Grigel <o.grigel83@yandex.ru>
2023-08-24 13:02:11 +0200

    Добавила несколько скриптов

---
+++ script.js
@@ -0,0 +1,16 @@
+<!-- Форма добавления нового ДУ -->
+
+function openWindowAdd(){
+    var windowAdd = document.querySelector(".add");
+    windowAdd.style.display = "flex";
+}
+
+function closeWindowAdd(){
+    var windowAdd = document.querySelector(".add");
+    windowAdd.style.display = "none";
+}
+
+<!-- Поиск по наименованию ДУ -->
+
+var cards=document.querySelector(".cards");
+var cardArr = document.querySelector(".card");
